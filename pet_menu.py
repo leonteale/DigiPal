bf6276845067ad7bffbebd8fc9718d47b886a792
Leon Teale <leonteale89@gmail.com>
2023-06-02 11:43:19 -0400

    Update

--- pet_menu.py
+++ pet_menu.py
@@ -10,7 +10,7 @@ def clear_screen():
     print(term.clear)
 
 # Function to display the menu
-def display_menu():
+def display_menu(box_height):
     menu_text = "Menu: 1. Info  |  2. Feed  |  3. Play  |  4. Exit"
     with term.location(0, box_height):
         print(term.black_on_white(menu_text))
